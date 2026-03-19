925d2cb6336410081c9169222a336a6414e31d24
CryptoManiacMayn <admin@oeconomia.io>
2026-03-19 07:19:48 -0700

    Fix unused import

--- src/components/layout/app-layout.tsx
+++ src/components/layout/app-layout.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Outlet, useOutletContext } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import { Sidebar } from "./sidebar";
 import { Header } from "./header";
 import { MobileNav } from "./mobile-nav";
